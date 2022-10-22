8295e4f100b0e93e69338297036e4ead5e26d773
Alex Billedeaux <alexbilledeaux@gmail.com>
2022-10-22 09:05:17 -0400

    Up through part 8

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import copy
+import traceback
 
 import tcod
 
+import color
 from engine import Engine
 import entity_factories
 from input_handlers import EventHandler
@@ -14,33 +16,38 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
 
     max_monsters_per_room = 2
+    max_items_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "src\dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
-
     player = copy.deepcopy(entity_factories.player)
 
-    game_map = generate_dungeon(
+    engine = Engine(player=player)
+
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        max_items_per_room=max_items_per_room,
+        engine=engine
     )
+    engine.update_fov()
 
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to the fabled CIS641 dungeon!", color.welcome_text
+    )
 
     with tcod.context.new_terminal(
         screen_width,
@@ -52,11 +59,18 @@ def main() -> None:
         # The order parameter sets numpy to use [x,y] instead of [y,x] format.
         root_console = tcod.Console(screen_width, screen_height, order = "F")
         while True:
-            engine.render(console=root_console, context=context)
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
 
-            events = tcod.event.wait()
-
-            engine.handle_events(events)
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception:  # Handle exceptions in game.
+                traceback.print_exc()  # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 if __name__ == "__main__":
     main()
